7496071126d037e1fc1c6f1c9ccb4310234195c9
Mattt Zmuda <mattt@loopwork.com>
2025-03-31 10:05:32 -0700

    Expand supported platforms

--- Package.swift
+++ Package.swift
@@ -7,7 +7,11 @@ let package = Package(
     name: "mcp-swift-sdk",
     platforms: [
         .macOS("13.0"),
+        .macCatalyst("16.0"),
         .iOS("16.0"),
+        .watchOS("9.0"),
+        .tvOS("16.0"),
+        .visionOS("1.0"),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
